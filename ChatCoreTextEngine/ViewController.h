ba0165a9d7ab4d4ffdb4081f27bccb3982b384c9
hehe19900512 <870347271@qq.com>
2015-03-23 20:15:24 +0800

    Initial Commit

---
+++ ChatCoreTextEngine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatCoreTextEngine
+//
+//  Created by 张海迪 on 15/3/23.
+//  Copyright (c) 2015年 haidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
